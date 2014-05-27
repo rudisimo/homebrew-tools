b1aee969801ec5e54552967bdc2dd911e2531aa1
Rodolfo Puig <rodolfo@puig.io>
2014-05-27 10:36:18 -0400

    Fixing an error in the svnbranch tarball URL

--- Formula/svnbranch.rb
+++ Formula/svnbranch.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Svnbranch < Formula
   homepage 'https://github.com/rudisimo/svnbranch'
-  url 'https://github.com/telemundo/svnbranch/archive/v0.2.0.tar.gz'
+  url 'https://github.com/rudisimo/svnbranch/archive/v0.2.0.tar.gz'
   sha1 '5f65dd732529fcde24f6d86ab85e20d861ece5d0'
 
   head 'https://github.com/rudisimo/svnbranch.git'
