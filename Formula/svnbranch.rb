d0999fe661b8f471507113ffb7ae380e2e87f9f9
Rodolfo Puig <rodolfo@puig.io>
2014-05-27 10:29:40 -0400

    Updating svnbranch to v0.2.0

--- Formula/svnbranch.rb
+++ Formula/svnbranch.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Svnbranch < Formula
-  homepage 'https://github.com/telemundo/svnbranch'
-  url 'https://github.com/telemundo/svnbranch/archive/v0.1.0.tar.gz'
-  sha1 '24ad488530084b05ee580b454afd3f1564218bdc'
+  homepage 'https://github.com/rudisimo/svnbranch'
+  url 'https://github.com/telemundo/svnbranch/archive/v0.2.0.tar.gz'
+  sha1 '5f65dd732529fcde24f6d86ab85e20d861ece5d0'
 
-  head 'https://github.com/telemundo/svnbranch.git'
+  head 'https://github.com/rudisimo/svnbranch.git'
 
   def install
     ENV['PREFIX'] = prefix
